67a34092b1f31ef2fc14bd146c64dad48af076a9
Mai-okn <maitel57rad@gmail.com>
2021-05-20 11:59:12 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,9 +3,21 @@ class PrototypesController < ApplicationController
   end
 
   def new
-
+    @prototype = Prototype.new
   end
 
-  def create_table
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path 
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def prototype_params
+    params.require(:prototype).permit(:title, :cacth_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
