052de82f230055bb34a8d3761b2e918a82f09a6d
Mai-okn <maitel57rad@gmail.com>
2021-05-20 23:42:49 +0900

    コメントモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :prototype
+  has_many :comments
 
   validates :name, presence: true
   validates :profile, presence: true
